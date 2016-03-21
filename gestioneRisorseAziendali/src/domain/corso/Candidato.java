81a72d9eb443510353560e526762dbead05d5e5c
AlessioGennari <genn.alessio@gmail.com>
2016-03-21 09:33:39 +0100

    Added files via upload

---
+++ gestioneRisorseAziendali/src/domain/corso/Candidato.java
@@ -0,0 +1,35 @@
+package corso;
+
+public class Candidato {
+	private String idCandidato;
+	private int idAnagrafica;
+	private String idRisorsa;
+	private int idCv;
+	
+	
+	public String getIdCandidato() {
+		return idCandidato;
+	}
+	public void setIdCandidato(String idCandidato) {
+		this.idCandidato = idCandidato;
+	}
+	public int getIdAnagrafica() {
+		return idAnagrafica;
+	}
+	public void setIdAnagrafica(int idAnagrafica) {
+		this.idAnagrafica = idAnagrafica;
+	}
+	public String getIdRisorsa() {
+		return idRisorsa;
+	}
+	public void setIdRisorsa(String idRisorsa) {
+		this.idRisorsa = idRisorsa;
+	}
+	public int getIdCv() {
+		return idCv;
+	}
+	public void setIdCv(int idCv) {
+		this.idCv = idCv;
+	}
+	
+}
