3645352ad568085d22aeeb49a91d0598c5c4c92c
danielangeli <danielangeli@outlook.com>
2016-03-21 09:52:38 +0100

    Added files via upload

---
+++ GestioneRisorseAziendali/src/domain/selezione/Selezione.java
@@ -0,0 +1,31 @@
+package domain.selezione;
+
+import java.time.LocalDate;
+
+public class Selezione {
+	private int idSelezione;
+	private LocalDate dataSelezione;
+	private int esito;
+	public Selezione() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getIdSelezione() {
+		return idSelezione;
+	}
+	public void setIdSelezione(int idSelezione) {
+		this.idSelezione = idSelezione;
+	}
+	public LocalDate getDataSelezione() {
+		return dataSelezione;
+	}
+	public void setDataSelezione(LocalDate dataSelezione) {
+		this.dataSelezione = dataSelezione;
+	}
+	public int getEsito() {
+		return esito;
+	}
+	public void setEsito(int esito) {
+		this.esito = esito;
+	}
+}
