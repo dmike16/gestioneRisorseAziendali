9304a2e4f584aa36396f1a4cc90a8863a0f050d9
SoniaMan <sonia.manarolla@gmail.com>
2016-03-21 11:32:02 +0100

    Update CV.java

--- GestioneRisorseAziendali/src/domain/anagrafica/CV.java
+++ GestioneRisorseAziendali/src/domain/anagrafica/CV.java
@@ -1,41 +1,44 @@
 package domain.anagrafica;
-
+import javax.validation.constraints.*;
 public class CV {
-private int idCV;
-private int idFormazione;
-private int idLavoro;
-private String urlImage;
-private int idLingua;
-public int getIdCV() {
-	return idCV;
-}
-public void setIdCV(int idCV) {
-	this.idCV = idCV;
-}
-public int getIdFormazione() {
-	return idFormazione;
-}
-public void setIdFormazione(int idFormazione) {
-	this.idFormazione = idFormazione;
-}
-public int getIdLavoro() {
-	return idLavoro;
-}
-public void setIdLavoro(int idLavoro) {
-	this.idLavoro = idLavoro;
-}
-public String getUrlImage() {
-	return urlImage;
-}
-public void setUrlImage(String urlImage) {
-	this.urlImage = urlImage;
-}
-public int getIdLingua() {
-	return idLingua;
-}
-public void setIdLingua(int idLingua) {
-	this.idLingua = idLingua;
-}
+	
+	@NotNull
+	@Digits(integer=6,fraction=0)
+	private int idCV;
+	private int idFormazione;
+	private int idLavoro;
+	private String urlImage;
+	private int idLingua;
+	public int getIdCV() {
+		return idCV;
+	}
+	public void setIdCV(int idCV) {
+		this.idCV = idCV;
+	}
+	public int getIdFormazione() {
+		return idFormazione;
+	}
+	public void setIdFormazione(int idFormazione) {
+		this.idFormazione = idFormazione;
+	}
+	public int getIdLavoro() {
+		return idLavoro;
+	}
+	public void setIdLavoro(int idLavoro) {
+		this.idLavoro = idLavoro;
+	}
+	public String getUrlImage() {
+		return urlImage;
+	}
+	public void setUrlImage(String urlImage) {
+		this.urlImage = urlImage;
+	}
+	public int getIdLingua() {
+		return idLingua;
+	}
+	public void setIdLingua(int idLingua) {
+		this.idLingua = idLingua;
+	}
 
 
 }
