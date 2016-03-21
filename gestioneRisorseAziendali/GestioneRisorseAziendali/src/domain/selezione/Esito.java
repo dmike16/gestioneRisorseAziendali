c9e6539e5528228887addc6d314bf24ccb2e3251
giorgio91 <gioteba@gmail.com>
2016-03-21 09:36:07 +0100

    Added files via upload

---
+++ gestioneRisorseAziendali/GestioneRisorseAziendali/src/domain/selezione/Esito.java
@@ -0,0 +1,30 @@
+package domain.selezione;
+
+public class Esito {
+	
+	private int idEsito;
+	private String valutazione;
+	private String descrizione;
+	
+	public int getIdEsito() {
+		return idEsito;
+	}
+	public void setIdEsito(int idEsito) {
+		this.idEsito = idEsito;
+	}
+	public String getValutazione() {
+		return valutazione;
+	}
+	public void setValutazione(String valutazione) {
+		this.valutazione = valutazione;
+	}
+	public String getDescrizione() {
+		return descrizione;
+	}
+	public void setDescrizione(String descrizione) {
+		this.descrizione = descrizione;
+	}
+	
+	
+
+}
