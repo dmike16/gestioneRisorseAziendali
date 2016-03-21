a33402f0aff02962e6f7da258ead5f2d6b9aff2b
vale89 <val.mazzarini@gmail.com>
2016-03-21 09:53:46 +0100

    Added files via upload

---
+++ GestioneRisorseAziendali/src/domain/selezione/Batteria.java
@@ -0,0 +1,21 @@
+package domain.selezione;
+
+public class Batteria {
+private String codiceBatteria;
+private String tipo;
+
+public String getCodiceBatteria() {
+	return codiceBatteria;
+}
+public void setCodiceBatteria(String codiceBatteria) {
+	this.codiceBatteria = codiceBatteria;
+}
+public String getTipo() {
+	return tipo;
+}
+public void setTipo(String tipo) {
+	this.tipo = tipo;
+}
+
+
+}
---
+++ GestioneRisorseAziendali/src/domain/selezione/Partecipa.java
@@ -0,0 +1,21 @@
+package domain.selezione;
+
+public class Partecipa {
+private String idCandidato;
+private  int idSelezione;
+
+
+public String getIdCandidato() {
+	return idCandidato;
+}
+public void setIdCandidato(String idCandidato) {
+	this.idCandidato = idCandidato;
+}
+public int getIdSelezione() {
+	return idSelezione;
+}
+public void setIdSelezione(int idSelezione) {
+	this.idSelezione = idSelezione;
+}
+
+}
