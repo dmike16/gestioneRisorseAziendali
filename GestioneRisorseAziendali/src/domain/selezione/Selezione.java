b8e1bf944799de7445a4c208c65abe2e613d19a6
danielangeli <danielangeli@outlook.com>
2016-03-21 11:36:56 +0100

    Update Selezione.java

--- GestioneRisorseAziendali/src/domain/selezione/Selezione.java
+++ GestioneRisorseAziendali/src/domain/selezione/Selezione.java
@@ -1,9 +1,14 @@
 package domain.selezione;
 
 import java.time.LocalDate;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Digits;
 
 public class Selezione {
+	@NotNull
+	@Digits(integer=6,fraction=0)
 	private int idSelezione;
+	
 	private LocalDate dataSelezione;
 	private int esito;
 	public Selezione() {
