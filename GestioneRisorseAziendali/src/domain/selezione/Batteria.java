e422582e4c4dec7c5fdb1410f9946fd5efbec889
danielangeli <danielangeli@outlook.com>
2016-03-21 11:33:29 +0100

    Update Batteria.java

--- GestioneRisorseAziendali/src/domain/selezione/Batteria.java
+++ GestioneRisorseAziendali/src/domain/selezione/Batteria.java
@@ -3,23 +3,21 @@ import javax.validation.constraints.NotNull;
 
 public class Batteria {
 	
-@NotNull
-private int codiceBatteria;
-
-private String tipo;
-
-public int getCodiceBatteria() {
-	return codiceBatteria;
-}
-public void setCodiceBatteria(int codiceBatteria) {
-	this.codiceBatteria = codiceBatteria;
-}
-public String getTipo() {
-	return tipo;
-}
-public void setTipo(String tipo) {
-	this.tipo = tipo;
-}
-
-
+	@NotNull
+	private int codiceBatteria;
+	
+	private String tipo;
+	
+	public int getCodiceBatteria() {
+		return codiceBatteria;
+	}
+	public void setCodiceBatteria(int codiceBatteria) {
+		this.codiceBatteria = codiceBatteria;
+	}
+	public String getTipo() {
+		return tipo;
+	}
+	public void setTipo(String tipo) {
+		this.tipo = tipo;
+	}
 }
