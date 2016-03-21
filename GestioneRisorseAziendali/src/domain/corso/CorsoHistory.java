f78cefc6818b8585e846a310a6d3a376cb0613d3
Alexi311 <alessandro.freschi93@gmail.com>
2016-03-21 09:57:11 +0100

    Added files via upload

---
+++ GestioneRisorseAziendali/src/domain/corso/CorsoHistory.java
@@ -0,0 +1,37 @@
+package domain.corso;
+
+public class CorsoHistory {
+	
+	private String idCorso;
+	private String idRisorsa;
+	
+	
+	public CorsoHistory() {
+		super();
+	}
+
+
+	public String getIdCorso() {
+		return idCorso;
+	}
+
+
+	public void setIdCorso(String idCorso) {
+		this.idCorso = idCorso;
+	}
+
+
+	public String getIdRisorsa() {
+		return idRisorsa;
+	}
+
+
+	public void setIdRisorsa(String idRisorsa) {
+		this.idRisorsa = idRisorsa;
+	}
+	
+	
+	
+	
+
+}
