379b0cf3023dfe56e8cc96438f562b3a7c01cf3a
dmike16 <cipmiky@gmail.com>
2016-03-21 10:01:59 +0100

    Aggiunti bean modulo + risorsa

---
+++ GestioneRisorseAziendali/src/domain/corso/Modulo.java
@@ -0,0 +1,30 @@
+package domain.corso;
+
+import java.time.LocalDate;
+
+public class Modulo {
+	private String idCorso;
+	private LocalDate dataInizio;
+	private LocalDate dataFine;
+	public String getIdCorso() {
+		return idCorso;
+	}
+	public void setIdCorso(String idCorso) {
+		this.idCorso = idCorso;
+	}
+	public LocalDate getDataInizio() {
+		return dataInizio;
+	}
+	public void setDataInizio(LocalDate dataInizio) {
+		this.dataInizio = dataInizio;
+	}
+	public LocalDate getDataFine() {
+		return dataFine;
+	}
+	public void setDataFine(LocalDate dataFine) {
+		this.dataFine = dataFine;
+	}
+	
+	
+	
+}
---
+++ GestioneRisorseAziendali/src/domain/corso/Risorsa.java
@@ -0,0 +1,20 @@
+package domain.corso;
+
+public class Risorsa {
+	private String idRisorsa;
+	private String idTirocinio;
+	public String getIdRisorsa() {
+		return idRisorsa;
+	}
+	public void setIdRisorsa(String idRisorsa) {
+		this.idRisorsa = idRisorsa;
+	}
+	public String getIdTirocinio() {
+		return idTirocinio;
+	}
+	public void setIdTirocinio(String idTirocinio) {
+		this.idTirocinio = idTirocinio;
+	}
+	
+	
+}
