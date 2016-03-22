72a92d2f8865afc60c0c961e008ddc94096b01e2
dmike16 <cipmiky@gmail.com>
2016-03-22 16:12:17 +0100

    Test chiamate ajax per ricerca dati anagrafici

--- GestioneRisorseAziendali/WebContent/js/RicercaDatiAnagrafici.js
+++ GestioneRisorseAziendali/WebContent/js/RicercaDatiAnagrafici.js
@@ -12,13 +12,33 @@
 	"use strict";
 	
 	jQuery(document).ready(function($){
-		$("[data-toggle=modal]").on('click',function(event){
+		$("button.elimina").on('click',function(event){
+			
 			$.ajax({
 				url: "testJson.action",
 				type: "GET",
 				dataType: "json",
 				success: function(response){
-					console.log("Dati: { bella: "+ response.bella +" }");
+					$('.modal-header').empty();
+					$('.modal-header').append("<h2>" + "Dati Candidato" + "</h2>");
+					
+					$('.modal-body').empty();
+					
+					var html=['<ul>'];
+					response.libri.forEach(function(element){
+						var s = "<li>"+element.titolo+"</li>"+
+						"<li>"+element.autore+"</li>"+
+						"<li>"+element.id+"</li>"+
+						"<li>"+element.prezzo+"</li>";
+						
+						html.push(s);
+					});
+					
+					$('.modal-body').append(html.concat(' '));
+					$('.modal-footer').empty();
+					$('.modal-footer').append('<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>' +
+							'<a href="cancellaDatiAnagrafici.action" class="btn btn-primary">Cancella</a>');
+					console.log(response.libri);
 				},
 				error: function(){
 					console.log("Error from server");
