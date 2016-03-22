d38da2273e62a304354653f740c435129c0a15a4
dmike16 <cipmiky@gmail.com>
2016-03-22 13:04:39 +0100

    Test ajax + java Servlet

--- GestioneRisorseAziendali/WebContent/js/RicercaDatiAnagrafici.js
+++ GestioneRisorseAziendali/WebContent/js/RicercaDatiAnagrafici.js
@@ -1,3 +1,29 @@
 /**
+ * Javascript per gestire le funzionalità
+ * di eliminazione + modifica + view + download Cv +
+ * dei candidati.
  * 
+ * Basta su tecnologia Jquery
  */
+
+//Progetto le variabili locali con una funzione 
+// che viene eseguita istantaneamente
+(function(){
+	"use strict";
+	
+	jQuery(document).ready(function($){
+		$("[data-toggle=modal]").on('click',function(event){
+			$.ajax({
+				url: "testJson.action",
+				type: "GET",
+				dataType: "json",
+				success: function(response){
+					console.log("Dati: { bella: "+ response.bella +" }");
+				},
+				error: function(){
+					console.log("Error from server");
+				}
+			});
+		});
+	});
+}());
