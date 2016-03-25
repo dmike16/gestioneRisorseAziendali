fbdc8503c623a495bcdfb8ef534940d433aa2fc6
dmike16 <cipmiky@gmail.com>
2016-03-25 10:51:54 +0100

    Colpa di eclipse

--- GestioneRisorseAziendali/bin/WebContent/js/GestioneDatiSelezione1.js
+++
@@ -1,6 +0,0 @@
-$('#submit').click(function () {
-    console.log('weee');
-    if (document.getElementById('batterieTest').checked) {
-        console.log(document.getElementById('batterieTest').value);
-    }
-});
