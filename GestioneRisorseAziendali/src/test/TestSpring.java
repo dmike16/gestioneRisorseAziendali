094c8057064c3b37d022f2e8289bdacd85b189e0
dmike16 <cipmiky@gmail.com>
2016-03-25 10:43:45 +0100

    Add JavaDoc Comment

--- GestioneRisorseAziendali/src/test/TestSpring.java
+++ GestioneRisorseAziendali/src/test/TestSpring.java
@@ -7,15 +7,33 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-
+/**
+ * Classe che rappresenta il controller usato da spring
+ * per gestire la ricerca dei candidati per dati anagrafici
+ *
+ *	@version 1.0
+ */
 @Controller
 public class TestSpring {
 	
-	
+	/**
+	 * Metodo usato per mappare l'action indexRicerca.action	
+	 * @return 			jsp che rappresenta la pagina iniziale della ricerca
+	 */
 	@RequestMapping("indexRicerca.action")
 	public String index(){
 		return "RicercaDatiAnagrafici";
 	}
+	
+	/**
+	 * Metodo che mappa il primo form di ricerca. Ricerca per nome e cognome.
+	 * Se sono entrambi vuoti genera un messaggio di errore
+	 * 
+	 * @param ricerca 			Mappa stringa,string dove la chiave è li name dell'input tag e il valore il corrispondete valore
+	 * @param model				Rappresenta l'interfaccia model definita da spring
+	 * @see <a href="https://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/ui/Model.html">Model</a>
+	 * @return					jsp che rappresenta la pagina iniziali della ricerca
+	 */
 	@RequestMapping("ricercaDatiAnagrafici1.action")
 	public String testFormSpring(@RequestParam Map<String,String> ricerca, Model model){
 		
@@ -28,6 +46,15 @@ public class TestSpring {
 		return "RicercaDatiAnagrafici";
 	}
 	
+	/**
+	 * Metodo che mappa il secondo form di ricerca. Ricerca per email.
+	 * Se è vuoto genera un errore
+	 * 
+	 * @param email		Il valore inserito nell' input tag con attributo {@code name="email"}
+	 * @param model		Rappresenta l'interfaccia model definita da spring
+	 * @see <a href="https://docs.spring.io/spring/docs/current/javadoc-api/org/springframework/ui/Model.html">Model</a>
+	 * @return
+	 */
 	@RequestMapping("ricercaDatiAnagrafici2.action")
 	public String testFromSpring(@RequestParam("email") String email, Model model){
 		
