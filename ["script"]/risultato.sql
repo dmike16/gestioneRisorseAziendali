28e3c3a403f68072e050a534068dc432b79b4f47
danielangeli <danielangeli@outlook.com>
2016-03-25 13:02:47 +0100

    Added files via upload

---
+++ ["script"]/risultato.sql
@@ -0,0 +1,9 @@
+INSERT INTO Risultato (idModulo,idRisorsa,esito) VALUES ('AKT000M',1,'Ammesso');
+INSERT INTO Risultato (idModulo,idRisorsa,esito) VALUES ('AKT000M',2,'Non ammesso');
+INSERT INTO Risultato (idModulo,idRisorsa,esito) VALUES ('AKT002M',1,'Non ammesso');
+INSERT INTO Risultato (idModulo,idRisorsa,esito) VALUES ('AKT002M',2,'Ammesso');
+INSERT INTO Risultato (idModulo,idRisorsa,esito) VALUES ('AKT001M',3,'Non ammesso');
+INSERT INTO Risultato (idModulo,idRisorsa,esito) VALUES ('AKT003M',5,'Ammesso');
+INSERT INTO Risultato (idModulo,idRisorsa,esito) VALUES ('AKT003M',6,'Non ammesso');
+INSERT INTO Risultato (idModulo,idRisorsa,esito) VALUES ('AKT002M',7,'Ammesso');
+INSERT INTO Risultato (idModulo,idRisorsa,esito) VALUES ('AKT002M',8,'Non ammesso');
