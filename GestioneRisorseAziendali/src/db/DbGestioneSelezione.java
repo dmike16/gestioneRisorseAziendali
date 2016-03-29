e360a0b841aa81ccd55d400ccebd842dc295da0e
giorgio91 <gioteba@gmail.com>
2016-03-29 10:34:57 +0200

    Added files via upload

--- GestioneRisorseAziendali/src/db/DbGestioneSelezione.java
+++ GestioneRisorseAziendali/src/db/DbGestioneSelezione.java
@@ -1,7 +1,6 @@
 package service;
 
 import java.sql.*;
-import java.sql.Connection;
 import java.util.ArrayList;
 
 import javax.naming.Context;
@@ -13,13 +12,33 @@ import service.ConnessioneService;
 
 
 @Service
-public class DbGestioneSelezione {
+public class DbGestioneSelezione implements ConnessioneService{
+
+	private Connection con;
+
+	public DbGestioneSelezione() throws ClassNotFoundException, SQLException, NamingException {
+		establishConnection();
+	}
+
+	// method used to establish connection with database
+	private void establishConnection() {
+
+		try {
+			Context initContext = new InitialContext();
+			Context envContext = (Context) initContext.lookup("java:comp/env");
+			DataSource ds = (DataSource) envContext.lookup("jdbc/hrdb");
+			con = ds.getConnection();
+		} catch (SQLException e) {
+			System.out.println("SQLException "+e);
+		} catch (NamingException e) {
+			System.out.println("NamingException "+e);
+		}
+	}
+
 	
-	private ConnessioneDb db ;
-	private Connection con = db.getConnection();
 
 	//dato un certo corsista si ottera la descrizione delle sue potenzialita
-	
+	@Override
 	public ArrayList<String> selectNomeCognomePotenzialita(String nome,String cognome)throws SQLException{
 		ArrayList<String> s= new ArrayList<>();
 		String sql = "select nome,cognome,dataNascita,email , potenzialita from AnagraficaCandidato a,Candidato c, "
@@ -46,7 +65,7 @@ public class DbGestioneSelezione {
 		return s;
 
 	}
-	
+	@Override
 	public ArrayList<String> selectNomeCognomeProfiloCaratteriale(String nome,String cognome)throws SQLException{
 		ArrayList<String> s= new ArrayList<>();
 
@@ -74,4 +93,8 @@ public class DbGestioneSelezione {
 
 	}
 	
+	
+		
+
+
 }
